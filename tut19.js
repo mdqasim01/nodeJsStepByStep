9534a1f91983ab13aa9b4077c553bb2e5ba2055c
Mohd Qasim <razaqasim123216@gmail.com>
2023-08-13 14:31:26 +0530

    tut 19

--- tut19.js
+++ tut19.js
@@ -1,15 +1,19 @@
 const express = require('express')
 const app = express()
 const router = express.Router();
+
 const urlCheck = require('./tut18B')
+
 app.get('/', function (req, res) {
-    res.send("Home Page")
+    // res.send("Home Page")
+    res.sendFile(__dirname+"/home.html")
 })
 router.get('/login', urlCheck, function (req, res) {
-    res.send("login Page")
+    res.sendFile(__dirname+"/login.html")
 })
 router.get('/about', urlCheck, function (req, res) {
-    res.send("About Page")
+    // res.send("About Page")
+    res.sendFile(__dirname+"/about.html")
 })
 app.use('/', router)
 app.listen(8080)
